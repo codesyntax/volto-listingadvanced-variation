31a8373de2175465658ca0cdcc2080f4ea3b9f08
ionlizarazu <ilizarazu@codesyntax.com>
2021-10-29 10:33:25 +0200

    add PropTypes and jsdoc

--- src/AdvancedListingBlockTemplate.jsx
+++ src/AdvancedListingBlockTemplate.jsx
@@ -9,6 +9,20 @@ import { isInternalURL } from '@plone/volto/helpers/Url/Url';
 import { Grid, Image } from 'semantic-ui-react';
 import moment from 'moment';
 import { useIntl } from 'react-intl';
+
+/**
+ * This is an advanced template for the default Volto listing block.
+ * @constructor
+ * @param {list} items - The list of items to render
+ * @param {bool} isEditMode - True if you are editing the block
+ * @param {string} linkTitle - The title to show the 'see more' link
+ * @param {string} linkHref - The href to set to the 'see more' link
+ * @param {string} imageSide - Where to put the image. 'up', 'right','down','left' or null (means without image)
+ * @param {number} imageWidth - How many columns (of 12) should the image occupy
+ * @param {number} howManyColumns - In how many columns will the items be displayed
+ * @param {bool} effectiveDate - If true, show the effective date of each item
+ * @param {function} titleTag - The function to render each item title
+ */
 const AdvancedListingBlockTemplate = ({
   items,
   linkTitle,
@@ -36,15 +50,19 @@ const AdvancedListingBlockTemplate = ({
   const hasImage = imageSide !== null;
   const oneColumnElement = ['up', 'down', null].includes(imageSide);
   const columnSize = oneColumnElement ? 1 : 2;
-  const imageGridWidth = oneColumnElement ? 12 : (imageWidth ? imageWidth : 2);
-  const contentGridWidth = oneColumnElement ? 12 : (hasImage ? 12 - imageWidth : 12);
+  const imageGridWidth = oneColumnElement ? 12 : imageWidth ? imageWidth : 2;
+  const contentGridWidth = oneColumnElement
+    ? 12
+    : hasImage
+    ? 12 - imageWidth
+    : 12;
   const intl = useIntl();
   moment.locale(intl.locale);
   return (
     <>
       <Grid columns={howManyColumns ? howManyColumns : 1} stretched>
         {items.map((item) => (
-          < Grid.Column key={item['@id']} >
+          <Grid.Column key={item['@id']}>
             <ConditionalLink item={item} condition={!isEditMode}>
               <Grid columns={columnSize}>
                 {['up', 'left'].includes(imageSide) && (
@@ -74,9 +92,7 @@ const AdvancedListingBlockTemplate = ({
                   ) : (
                     <h3>{item.title ? item.title : item.id}</h3>
                   )}
-                  {effectiveDate &&
-                    <p>{moment(item.effective).format("L")}</p>
-                  }
+                  {effectiveDate && <p>{moment(item.effective).format('L')}</p>}
                   <p>{item.description}</p>
                 </Grid.Column>
                 {['right', 'down'].includes(imageSide) && (
@@ -112,8 +128,14 @@ const AdvancedListingBlockTemplate = ({
 
 AdvancedListingBlockTemplate.propTypes = {
   items: PropTypes.arrayOf(PropTypes.any).isRequired,
-  linkMore: PropTypes.any,
   isEditMode: PropTypes.bool,
+  linkTitle: PropTypes.string,
+  linkHref: PropTypes.string,
+  imageSide: PropTypes.string,
+  imageWidth: PropTypes.number,
+  howManyColumns: PropTypes.number,
+  effectiveDate: PropTypes.bool,
+  titleTag: PropTypes.func,
 };
 
 export default AdvancedListingBlockTemplate;
