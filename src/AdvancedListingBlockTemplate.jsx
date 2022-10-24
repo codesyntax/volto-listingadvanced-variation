efdf29aa67a0d48d828fd7446e7becb216f0f2d2
rnunez80 <rnunez@york.cuny.edu>
2022-10-24 12:21:32 -0400

    Update AdvancedListingBlockTemplate.jsx
    Event details options

--- src/AdvancedListingBlockTemplate.jsx
+++ src/AdvancedListingBlockTemplate.jsx
@@ -1,28 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ConditionalLink } from '@plone/volto/components';
-import { flattenToAppURL } from '@plone/volto/helpers';
+import {ConditionalLink} from '@plone/volto/components';
+import {flattenToAppURL} from '@plone/volto/helpers';
 
 import DefaultImageSVG from '@plone/volto/components/manage/Blocks/Listing/default-image.svg';
-import { isInternalURL } from '@plone/volto/helpers/Url/Url';
-import { Grid, Image } from 'semantic-ui-react';
+import {isInternalURL} from '@plone/volto/helpers/Url/Url';
+import {Grid, Image} from 'semantic-ui-react';
 import moment from 'moment';
-import { useIntl } from 'react-intl';
+import {useIntl} from 'react-intl';
+
 const AdvancedListingBlockTemplate = ({
-  items,
-  moreLinkText,
-  moreLinkUrl,
-  header,
-  headerUrl,
-  headerTag,
-  isEditMode,
-  imageSide,
-  imageWidth,
-  howManyColumns,
-  effectiveDate,
-  titleTag,
-  showDescription,
-}) => {
+                                        items,
+                                        moreLinkText,
+                                        moreLinkUrl,
+                                        header,
+                                        headerUrl,
+                                        headerTag,
+                                        isEditMode,
+                                        imageSide,
+                                        imageWidth,
+                                        howManyColumns,
+                                        effectiveDate,
+                                        titleTag,
+                                        showDescription,
+                                        eventDate,
+                                        eventLocation,
+                                        eventTime
+                                      }) => {
   let moreLink = null;
   let moreHref = moreLinkUrl?.[0]?.['@id'] || '';
   if (isInternalURL(moreHref)) {
@@ -46,7 +50,49 @@ const AdvancedListingBlockTemplate = ({
   } else if (headerHref) {
     moreLink = <a href={headerHref}>{moreLinkText || headerHref}</a>;
   }
+  const getEventDate = (item) => {
+    let start = '',
+      end = '';
 
+    if (item.start) {
+      const parsedDate = new Date(Date.parse(item.start));
+      start = `${parsedDate.toLocaleString('default', {
+        month: 'short',
+      })} ${parsedDate.getDate()}, ${parsedDate.getFullYear()}`;
+    }
+
+    if (item.end) {
+      const parsedDate = new Date(Date.parse(item.end));
+      end = `${parsedDate.toLocaleString('default', {
+        month: 'short',
+      })} ${parsedDate.getDate()}, ${parsedDate.getFullYear()}`;
+    }
+    if (end == start) {return start}
+    else {return start + ' - ' + end;};
+  };
+  const getEventTime = (item) => {
+    let start = '',
+      end = '';
+
+    if (item.start) {
+      const parsedDate = new Date(Date.parse(item.start));
+      start = `${parsedDate.toLocaleString(
+        'en-US',
+        {hour: 'numeric', minute: 'numeric', hour12: true},
+      )}`;
+    }
+
+    if (item.end) {
+      const parsedDate = new Date(Date.parse(item.end));
+      end = ` - ${parsedDate.toLocaleString('en-US', {
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true,
+      })}`;
+    }
+
+    return start + end;
+  };
   const hasImage = imageSide !== null;
   const oneColumnElement = ['up', 'down', null].includes(imageSide);
   const columnSize = oneColumnElement ? 1 : 2;
@@ -54,8 +100,8 @@ const AdvancedListingBlockTemplate = ({
   const contentGridWidth = oneColumnElement
     ? 12
     : hasImage
-    ? 12 - imageWidth
-    : 12;
+      ? 12 - imageWidth
+      : 12;
   const intl = useIntl();
   const TitleTag = titleTag ? titleTag : 'h3';
   const HeaderTag = headerTag ? headerTag : 'h3';
@@ -99,10 +145,15 @@ const AdvancedListingBlockTemplate = ({
                     {item.title ? item.title : item.id}
                   </ConditionalLink>
                 </TitleTag>
+                {eventDate|eventTime && <p class="event-when">{eventDate && <span className="start-date">{getEventDate(item)}</span>}
+                {eventTime && eventDate && <span> | </span>}
+                  {eventTime && <span className="start-time">{getEventTime(item)}</span>}</p>||null}
+                {eventLocation && <p>{item.location}</p>}
                 {effectiveDate && <p>{moment(item.effective).format('L')}</p>}
                 {showDescription && item.description && (
                   <p>{item.description}</p>
                 )}
+
               </Grid.Column>
               {['right', 'down'].includes(imageSide) && (
                 <Grid.Column width={imageGridWidth}>
